93a6a8363e819405b6311ffef9bc5c347f6f663e
adnilson-quitembo <adnilsonmitcho@gmail.com>
2025-04-12 23:54:15 +0100

    mundaça da api

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://00000.loca.lt/', // URL base da sua API
+  baseURL: 'https://little-jellyfish-83.telebit.io/', // URL base da sua API
   timeout: 5000, // Timeout de 5 segundos
   headers: {
     'Content-Type': 'application/json',
